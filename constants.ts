08edc7d6b9597b93619fcc7707d0ecc1d970c651
Ryan210 <286661636@qq.com>
2023-12-31 16:49:55 +0800

    decor

--- constants.ts
+++ constants.ts
@@ -1,39 +1,42 @@
-import { Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+import { VideotapeIcon } from "lucide-react";
+import { MessageCircle } from "lucide-react";
+import { Code, Code2, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+import { MessageChannel } from "worker_threads";
 
 export const MAX_FREE_COUNTS = 5;
 
 export const tools = [
   {
-    label: 'Conversation',
+    label: 'Conversation Gift',
     icon: MessageSquare,
     href: '/conversation',
     color: "text-violet-500",
     bgColor: "bg-violet-500/10",
   },
   {
-    label: 'Music Generation',
+    label: 'Music Gift',
     icon: Music,
     href: '/music',
     color: "text-emerald-500",
     bgColor: "bg-emerald-500/10",
   },
   {
-    label: 'Image Generation',
+    label: 'Image Gift',
     icon: ImageIcon,
     color: "text-pink-700",
     bgColor: "bg-pink-700/10",
     href: '/image',
   },
   {
-    label: 'Video Generation',
-    icon: VideoIcon,
+    label: 'Video Gift',
+    icon: VideotapeIcon,
     color: "text-orange-700",
     bgColor: "bg-orange-700/10",
     href: '/video',
   },
   {
-    label: 'Code Generation',
-    icon: Code,
+    label: 'Code Gift',
+    icon: Code2,
     color: "text-green-700",
     bgColor: "bg-green-700/10",
     href: '/code',
